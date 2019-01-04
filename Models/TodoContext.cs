adf812c81b4cccb5ccd3dceb3b9a162626f8a226
Jose Jaime Figueroa Barrios <jaimefigueroa84@gmail.com>
2019-01-04 12:09:44 -0700

    Add Get Methods

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -4,9 +4,9 @@ namespace TodoApi.Models
 {
     public class TodoContext: DbContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options)
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
         {
-            
+        
         }
         public DbSet<TodoItem> TodoItems { get; set; }
     }
